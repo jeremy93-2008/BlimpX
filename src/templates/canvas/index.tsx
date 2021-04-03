4a19c4c38e6081bc3caf74eb7129ede306d06fea
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2021-04-03 12:22:02 +0200

    Some Refactor

--- src/templates/canvas/index.tsx
+++ src/templates/canvas/index.tsx
@@ -1,19 +1,19 @@
 import React, {useEffect, useRef, useState} from "react";
 
 import "./canvas.scss"
-import {BlimpStage} from "./blimpStage";
+import {BlimpStage} from "./component/blimpStage";
 
 export function Canvas() {
     const refCanvas = useRef<HTMLDivElement>(null);
-    const [{ width, height }, setCanvasSize] = useState({ width: 0, height: 0});
+    const [{width, height}, setCanvasSize] = useState({width: 0, height: 0});
 
     useEffect(() => {
-        if(!refCanvas.current) return;
+        if (!refCanvas.current) return;
         const onResize = () => {
             const newWidth = refCanvas.current!.offsetWidth / 1.5
             const newHeight = refCanvas.current!.offsetHeight / 1.5
             // We check if the width and height are not the same to avoid useless render
-            if(width + height == newWidth + newHeight) return;
+            if (width + height == newWidth + newHeight) return;
             setCanvasSize({width: newWidth, height: newHeight})
         }
 
@@ -27,7 +27,7 @@ export function Canvas() {
 
     return (
         <div ref={refCanvas} className="canvas-container">
-            <BlimpStage width={width} height={height} />
+            <BlimpStage width={width} height={height}/>
         </div>
     )
 }
